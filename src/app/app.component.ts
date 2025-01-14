84e3776ec3c13548908299725903f5ae95463c9e
Cooljeb <joseph.brou@gmail.com>
2025-01-14 15:56:23 +0100

    P2 signals suite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RestaurantComponent } from "./components/restaurant/restaurant.component";
 import { ConnexionComponent } from './components/connexion/connexion.component';
+import { SignalsCptComponent } from './components/signals-cpt/signals-cpt.component';
+import { BiblioComponent } from "./components/biblio/biblio.component";
 
 
 
@@ -9,9 +11,11 @@ import { ConnexionComponent } from './components/connexion/connexion.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RestaurantComponent,ConnexionComponent],
+  imports: [RestaurantComponent, ConnexionComponent, SignalsCptComponent, BiblioComponent],
   //imports: [TdlComponent]
 })
 export class AppComponent {
   title = signal('Mon titre');
+  nb = signal(10);//signal de 10
+  double=computed(()=> this.nb() * 2);
 }
